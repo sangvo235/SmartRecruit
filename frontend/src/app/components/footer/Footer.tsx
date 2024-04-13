b98e27418ec3b88fd3261de465512918c2f67a84
sangvo235 <sangvo235@gmail.com>
2024-04-14 06:45:52 +1000

    update

---
+++ frontend/src/app/components/footer/Footer.tsx
@@ -0,0 +1,43 @@
+import { Phone, Instagram, Linkedin, Facebook, Youtube } from 'lucide-react';
+
+const Footer = () => {
+    return (
+        <footer className="mb-24">
+            <div className="padding-container max-container flex w-full flex-col gap-14"> 
+            
+            
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
+
+
+// import React from 'react'
+// import { AiFillLinkedin, AiFillGithub, AiFillMail } from 'react-icons/ai';
+
+// export const Footer = () => {
+//   return (
+//     <footer className='mx-auto maw-w-3xl px-4 sm:px-6 md:px-6 md:max-w-5xl'>
+//         <hr className='w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0'></hr>
+//         <div className='mx-auto p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between'>
+//             <div className='text-neutral-500 dark:text-neutral-100'>
+//                 &#169; 2023 Sang Vo
+//             </div>
+//             <div className='flex flex-row items-center justify-center space-x-2 text-2xl text-neutral-500 dark:text-neutral-100'>
+//                 <a href="https://www.linkedin.com/in/sangvo235/" target="_blank" title="linkedin">
+//                     <AiFillLinkedin className="hover:text-sky-700 hover:-translate-y-1 transition-transform cursor-pointer"/>
+//                 </a>
+//                 <a href="https://github.com/sangvo235">
+//                     <AiFillGithub className="hover:text-sky-700 hover:-translate-y-1 transition-transform cursor-pointer" target="_blank" title="github"/>
+//                 </a>
+//                 <a href="mailto:sangvo235@gmail.com" target="_blank" title="email">
+//                     <AiFillMail className="hover:text-sky-700 hover:-translate-y-1 transition-transform cursor-pointer"/>
+//                 </a>
+//             </div>
+//         </div>
+//     </footer>
+//   )
+// }
+
