c9b5ee223bf12f11ace3f11f914772999e55d5d6
sangvo235 <sangvo235@gmail.com>
2024-04-14 07:20:58 +1000

    footer-content-updated

--- frontend/src/app/components/footer/Footer.tsx
+++ frontend/src/app/components/footer/Footer.tsx
@@ -1,43 +1,36 @@
-import { Phone, Instagram, Linkedin, Facebook, Youtube } from 'lucide-react';
+import { AiFillMail, AiFillPhone, AiFillLinkedin, AiFillInstagram, AiFillFacebook, AiFillYoutube } from 'react-icons/ai';
 
 const Footer = () => {
     return (
-        <footer className="mb-24">
-            <div className="padding-container max-container flex w-full flex-col gap-14"> 
-            
-            
+    <footer className='mx-auto max-w-7xl px-4 sm:px-6 md:px-6'>
+         <hr className='w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0'></hr>
+          <div className='mx-auto p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between'>
+                <div className='text-neutral-500 dark:text-neutral-100 text-lg'>
+                  &#169; 2024 Smart Recruit Project Group AC4 - Swinburne University of Technology
+                </div>
+                <div className='flex flex-row items-center justify-center space-x-2 text-4xl text-neutral-500 dark:text-neutral-100'>
+                    <a href="mailto:info@smaart.com.au" title="email">
+                        <AiFillMail className="hover:text-smartorange hover:-translate-y-1 transition-transform cursor-pointer"/>
+                    </a> 
+                    <a href="tel:1300762278" title="telephone">
+                        <AiFillPhone className="hover:text-smartorange hover:-translate-y-1 transition-transform cursor-pointer"/>
+                    </a>
+                    <a href="https://www.linkedin.com/company/smaart-recruitment/" title="linkedin">
+                        <AiFillLinkedin className="hover:text-smartorange hover:-translate-y-1 transition-transform cursor-pointer"/>
+                    </a>
+                    <a href="https://www.instagram.com/smaart_recruitment/" title="instagram">
+                        <AiFillInstagram className="hover:text-smartorange hover:-translate-y-1 transition-transform cursor-pointer"/>
+                    </a>
+                    <a href="https://www.facebook.com/smaartrecruitment" title="facebook">
+                        <AiFillFacebook className="hover:text-smartorange hover:-translate-y-1 transition-transform cursor-pointer"/>
+                    </a>
+                    <a href="http://www.youtube.com/@smaarttv8899" title="youtube">
+                        <AiFillYoutube className="hover:text-smartorange hover:-translate-y-1 transition-transform cursor-pointer"/>
+                    </a>
+                </div>
             </div>
         </footer>
     );
 }
 
 export default Footer;
-
-
-// import React from 'react'
-// import { AiFillLinkedin, AiFillGithub, AiFillMail } from 'react-icons/ai';
-
-// export const Footer = () => {
-//   return (
-//     <footer className='mx-auto maw-w-3xl px-4 sm:px-6 md:px-6 md:max-w-5xl'>
-//         <hr className='w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0'></hr>
-//         <div className='mx-auto p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between'>
-//             <div className='text-neutral-500 dark:text-neutral-100'>
-//                 &#169; 2023 Sang Vo
-//             </div>
-//             <div className='flex flex-row items-center justify-center space-x-2 text-2xl text-neutral-500 dark:text-neutral-100'>
-//                 <a href="https://www.linkedin.com/in/sangvo235/" target="_blank" title="linkedin">
-//                     <AiFillLinkedin className="hover:text-sky-700 hover:-translate-y-1 transition-transform cursor-pointer"/>
-//                 </a>
-//                 <a href="https://github.com/sangvo235">
-//                     <AiFillGithub className="hover:text-sky-700 hover:-translate-y-1 transition-transform cursor-pointer" target="_blank" title="github"/>
-//                 </a>
-//                 <a href="mailto:sangvo235@gmail.com" target="_blank" title="email">
-//                     <AiFillMail className="hover:text-sky-700 hover:-translate-y-1 transition-transform cursor-pointer"/>
-//                 </a>
-//             </div>
-//         </div>
-//     </footer>
-//   )
-// }
-
