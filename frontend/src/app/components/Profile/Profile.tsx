91f99bc55fb50e24fd4682ac2bf5710d79e30c6e
sangvo235 <sangvo235@gmail.com>
2024-04-14 21:59:50 +1000

    profile-component-update

--- frontend/src/app/components/Profile/Profile.tsx
+++ frontend/src/app/components/Profile/Profile.tsx
@@ -2,6 +2,8 @@ import { Avatar, AvatarFallback, AvatarImage } from "../Avatar/Avatar";
 import { Label } from "../Label/Label";
 import { useEffect, useState } from "react";
 import { AVATAR_LIST } from '../../../../constants';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "../DropDownMenu/DropDownMenu";
+import Link from 'next/link';
 
 const Profile = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -16,13 +18,25 @@ const Profile = () => {
     }, []);
 
     return (
-            <div className="inline-flex items-center text-center gap-2">
-                <Label className="text-lg font-semibold">{AVATAR_LIST[currentImageIndex].name}</Label>
-                <Avatar>
-                    <AvatarImage src={AVATAR_LIST[currentImageIndex].src} />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-            </div>
+       
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <div className="inline-flex items-center text-center gap-2">
+                    <Label className="text-lg font-semibold">{AVATAR_LIST[currentImageIndex].name}</Label>
+                    <Avatar>
+                        <AvatarImage src={AVATAR_LIST[currentImageIndex].src} />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>Settings</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <Link href="/pages/account"><DropdownMenuItem>Account Details</DropdownMenuItem></Link>
+                <DropdownMenuItem>Log Out</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+ 
     );
 }
 
