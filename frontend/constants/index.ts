c970c50bf40735236b40c1f1de89e18e98091c7b
sangvo235 <sangvo235@gmail.com>
2024-04-14 21:11:29 +1000

    nav-reorg

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,7 +1,7 @@
 // NAVBAR
 export const NAV_LINKS = [
     { href: '/', key: 'home', label: 'Home' },
-    { href: '/job', key: 'job', label: 'Job Listing' },
-    { href: '/upload', key: 'upload', label: 'Upload Resume' },
-    { href: '/invite', key: 'invite', label: 'Invitations' },
+    { href: '/pages/job', key: 'job', label: 'Job Listing' },
+    { href: '/pages/upload', key: 'upload', label: 'Upload Resume' },
+    { href: '/pages/invite', key: 'invite', label: 'Invitations' },
 ]
