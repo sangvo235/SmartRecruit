0d0cf2a6e4de7dfa9dd1339045ba56823131a401
sangvo235 <sangvo235@gmail.com>
2024-04-15 18:34:20 +1000

    page-creation

---
+++ frontend/src/app/pages/online-assessment/[id]/page.tsx
@@ -0,0 +1,9 @@
+const JobListingPage = () => {
+    return (
+        <main className="max-w-[1500px] m-auto p-6">
+        Online Assessment Page dynamic
+        </main>
+    )
+}
+
+export default JobListingPage;
---
+++ frontend/src/app/pages/online-assessment/page.tsx
@@ -0,0 +1,9 @@
+const JobListingPage = () => {
+    return (
+        <main className="max-w-[1500px] m-auto p-6">
+        Online Assessment Default Page
+        </main>
+    )
+}
+
+export default JobListingPage;
