1def8224fda0f493e96eb84c8cad2a3d8003baf4
sangvo235 <sangvo235@gmail.com>
2024-04-17 06:36:03 +1000

    joblist-fetch-data

--- frontend/src/app/components/molecules/JobList/JobList.tsx
+++ frontend/src/app/components/molecules/JobList/JobList.tsx
@@ -3,5 +3,30 @@
 import { useEffect, useState } from "react"
 
 const JobList = () => {
-    return ()
+    const getJobs = async () => {
+        const url = 'http://localhost:8000/api/jobs/'
+
+        await fetch(url, {
+            method: 'GET',
+        })
+            .then(response => response.json())
+            .then((json) => {
+                console.log('json', json);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        };
+
+    useEffect(() => {
+        getJobs();
+    }, []);
+
+    return (
+        <div>
+            <h1>Info from db will be displayed below, component still WIP:</h1>
+        </div>
+    )
 }
+
+export default JobList;
--- frontend/src/app/pages/job/page.tsx
+++ frontend/src/app/pages/job/page.tsx
@@ -1,7 +1,10 @@
+import JobList from "@/app/components/molecules/JobList/JobList";
+
 const JobListingPage = () => {
     return (
         <main className="max-w-[1500px] m-auto p-6">
         Job Listing Page
+        <JobList />
         </main>
     )
 }
