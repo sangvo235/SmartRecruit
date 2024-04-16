166034a08b0925a2dde8eb3efb1c6aeebf6de66e
sangvo235 <sangvo235@gmail.com>
2024-04-16 23:45:22 +1000

    job-model-updated

--- backend/smartrecruit_backend/job/models.py
+++ backend/smartrecruit_backend/job/models.py
@@ -1,3 +1,16 @@
+import uuid 
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from useraccount.models import User
+
+class Job(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    salary = models.ImageField()
+    location = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    recruiter = models.ForeignKey(User, related_name='jobs', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
