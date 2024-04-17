e786707cb70886042772fa5bdbd7f04ee35a6cfa
sangvo235 <sangvo235@gmail.com>
2024-04-17 21:40:40 +1000

    title-change

--- frontend/src/app/pages/online-assessment/page.tsx
+++ frontend/src/app/pages/online-assessment/page.tsx
@@ -1,7 +1,7 @@
 const OnlineAssessmentPage = () => {
     return (
         <main className="max-w-[1500px] m-auto p-6">
-        Online Assessment Page dynamic - tbc after connecting to DB
+        Online Assessment Page
         </main>
     )
 }
