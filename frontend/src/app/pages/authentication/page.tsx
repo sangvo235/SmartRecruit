da694499636d70635cbc55f5600c9e6e397424b0
sangvo235 <sangvo235@gmail.com>
2024-04-21 18:41:20 +1000

    code-clean-up

--- frontend/src/app/pages/authentication/page.tsx
+++ frontend/src/app/pages/authentication/page.tsx
@@ -7,7 +7,6 @@ const AuthenticationPage = () => {
         <main className="max-w-[1250px] m-auto p-6">
             <div className="grid grid-cols-2 gap-6">
                 <AuthenticationTab /> 
-
                 <div className="col-span-1 flex justify-center items-center">
                     <Image src="/hot-air-balloon.gif" alt="logo" width={400} height={400} />
                 </div>
