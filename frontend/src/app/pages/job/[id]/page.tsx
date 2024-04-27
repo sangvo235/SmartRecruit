930073c2e8fda17efc332df35caae8992e4461a3
sangvo235 <sangvo235@gmail.com>
2024-04-28 00:41:30 +1000

    page-title-change

--- frontend/src/app/pages/job/[id]/page.tsx
+++ frontend/src/app/pages/job/[id]/page.tsx
@@ -2,8 +2,6 @@
 import FullJobDetails from "@/app/components/molecules/FullJobDetails/FullJobDetails";
 
 const JobDetailsPage = async () => {
-    // const userId = await getUserId();
-
     return (
         <main className="max-w-[1500px] m-auto p-6">
             <FullJobDetails id={""} title={""} contract_type={""} company={""} location={""} industry={""} salary={0} description={""} intro={""} image_url={""} recruiter={""} recruiter_name={""} recruiter_email={""} created_at={""} />
--- frontend/src/app/pages/online-assessment/page.tsx
+++ frontend/src/app/pages/online-assessment/page.tsx
@@ -1,7 +1,7 @@
 const OnlineAssessmentPage = () => {
     return (
         <main className="max-w-[1500px] m-auto p-6">
-        Online Assessment Page
+            <div className="text-5xl font-semibold leading-none tracking-tight text-center pb-8">Online Assessment Page</div>
         </main>
     )
 }
