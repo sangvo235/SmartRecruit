dd409bcc97dfb202029b2b12b060c44c6f092aca
sangvo235 <sangvo235@gmail.com>
2024-04-28 17:14:44 +1000

    building-models-online-assessment

---
+++ backend/smartrecruit_backend/online_assessment/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Assessment(models.Model):
+    name = models.CharField(max_length=100)
+    topic = models.CharField(max_length=100)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text="Duration in minutes")
+    required_score_to_pass = models.IntegerField(help_text="Required score in %")
+
+    def __str__(self):
+        return f'{self.name} - {self.topic}'
+    
+    def get_questions(self):
+        return self.questions_set.all()
+
---
+++ backend/smartrecruit_backend/results/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from online_assessment.models import Assessment
+from useraccount.models import User
+
+# Create your models here.
+
+class Result(models.Model):
+    assessment = models.ForeignKey(Assessment, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.FloatField()
+
+    def __str__(self):
+        return str(self.pk)
