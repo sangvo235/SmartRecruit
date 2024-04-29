5a395812879455d8fcb475759a214caabd850b96
sangvo235 <sangvo235@gmail.com>
2024-04-30 04:01:12 +1000

    job-model-clean-up

--- backend/smartrecruit_backend/job/models.py
+++ backend/smartrecruit_backend/job/models.py
@@ -1,8 +1,10 @@
+import uuid 
 from django.conf import settings
 from django.db import models
 from useraccount.models import User
 
 class Job(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     contract_type = models.CharField(max_length=255)
     company = models.CharField(max_length=255)
@@ -13,10 +15,7 @@ class Job(models.Model):
     description = models.TextField()
     recruiter = models.ForeignKey(User, related_name='jobs', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='uploads/jobs', default='uploads/jobs/default.jpg')
+    image = models.ImageField(upload_to='uploads/jobs')
 
     def image_url(self):
-        if self.image:
-            return f'{settings.WEBSITE_URL}{self.image.url}'
-        else: 
-            return ''
+        return f'{settings.WEBSITE_URL}{self.image.url}'
