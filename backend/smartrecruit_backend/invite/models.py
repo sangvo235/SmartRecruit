b6428232cc8cd9c9287813184294bfbc09999e7c
sangvo235 <sangvo235@gmail.com>
2024-04-30 18:13:51 +1000

    add-model

---
+++ backend/smartrecruit_backend/invite/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from online_assessment.models import Assessment
+from useraccount.models import User
+
+class Invite(models.Model):
+    user_id = models.ForeignKey(User, related_name='invites', on_delete=models.CASCADE)
+    assessment_id = models.ForeignKey(Assessment, related_name='invites', on_delete=models.CASCADE)
+    invite_date = models.DateTimeField(auto_now_add=True)
+    expire_date = models.DateTimeField()
+    expired = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user_id} - {self.assessment_id}'
+
