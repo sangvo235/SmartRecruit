d8a793e195fb96ea9b0916ef64431241a27a6089
sangvo235 <sangvo235@gmail.com>
2024-05-05 05:12:22 +1000

    fixing-social-auth-issue

--- backend/smartrecruit_backend/online_assessment/api.py
+++ backend/smartrecruit_backend/online_assessment/api.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse   
+# from backend.smartrecruit_backend.questions.models import Answer
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from .models import Assessment
 from .serializers import AssessmentSerializer
@@ -27,6 +28,39 @@ def asessement_question_answer_data(request, pk):
         'time': assessments.time,
         'name' : assessments.name,
     })
+
+# @api_view(['POST'])
+# @authentication_classes([])
+# @permission_classes([])
+# def save_asessement(request, pk):
+#     print(request.POST)
+#     user = request.user
+#     assessment = Assessment.objects.get(pk=pk)
+
+#     score = 0
+#     multiplier = 100 / assessment.number_of_questions
+#     results = []
+#     correct_answer = None
+    
+#     for q in questions:
+#         a_selected = request.POST.get(q.text)
+
+#         if a_selected != "":
+#             question_answers = Answer.objects.filter(question=q)
+#             for a in question_answers:
+#                 if a_selected == a.text:
+#                     if a.correct:
+#                         score += 1
+#                         correct_answer = a.text
+#                     else:
+#                         if a.correct: 
+#                             correct_answer = a.text
+#             results.append({str(q): {'correct_answer': correct_answer, 'answer': a_selected}})
+#         else:
+#             results.append({str(q): 'not answered'})
+
+#     return JsonResponse({'text': 'success'})
+
 
 @api_view(['POST'])
 @authentication_classes([])
--- backend/smartrecruit_backend/smartrecruit_backend/settings.py
+++ backend/smartrecruit_backend/smartrecruit_backend/settings.py
@@ -83,6 +83,7 @@ INSTALLED_APPS = [
 
     'dj_rest_auth',
     'dj_rest_auth.registration',
+    'allauth.socialaccount',
 
     'corsheaders',
 
