bd2ecf3022a9f0e39aa40011161d555b9b4548cd
sangvo235 <sangvo235@gmail.com>
2024-05-15 04:48:24 +1000

    remove-unused-api-urls

--- backend/smartrecruit_backend/ml_processing/urls.py
+++ backend/smartrecruit_backend/ml_processing/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
-    # path('process/', api.process_resume, name='process_resume'),
-    path('job_description/', api.parse_job_description, name='parse_job_description'),
     path('match/', api.match_resume, name='match_resume'),
 ]
