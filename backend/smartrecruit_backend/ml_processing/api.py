0ba976782617c4fc491fcb5158761ce90fc11a5e
sangvo235 <sangvo235@gmail.com>
2024-05-15 04:48:50 +1000

    api-remove-job-processing

--- backend/smartrecruit_backend/ml_processing/api.py
+++ backend/smartrecruit_backend/ml_processing/api.py
@@ -5,30 +5,6 @@ from useraccount.models import User
 
 resume_processor = ResumeProcessor()
 
-@api_view(['PUT'])
-@authentication_classes([])
-@permission_classes([])
-def parse_job_description(request):
-    if request.method == 'PUT':
-        try:
-            job_description_text = request.data.get('job_description', '')
-
-            if not job_description_text:
-                return Response({"error": "Job description text is required"}, status=400)
-
-            # Clean and extract skills
-            cleaned_text = resume_processor.clean_text(job_description_text)
-            job_description_skills = resume_processor.extract_skills(cleaned_text)
-
-            # Store it in the database
-            job_desc = JobDescription.objects.create(text=job_description_text, skills=job_description_skills)
-            
-            return Response({"id": job_desc.id, "skills": job_description_skills})
-
-        except Exception as e:
-            return Response({"error": str(e)}, status=500)
-    return Response({'error': 'Method not allowed'}, status=405)
-
 @api_view(['POST'])
 @authentication_classes([])
 @permission_classes([])
