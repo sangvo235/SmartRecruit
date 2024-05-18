c4a47a22cf1fc1fdbafa300efbe3ecaba1c33271
sangvo235 <sangvo235@gmail.com>
2024-05-18 15:16:04 +1000

    removed-secure-cookies

--- frontend/src/app/lib/actions.ts
+++ frontend/src/app/lib/actions.ts
@@ -45,19 +45,22 @@ export async function handleRefresh() {
 export async function handleLogin(userId: string, accessToken: string, refreshToken: string) {
     cookies().set("session_userid", userId, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        // secure: process.env.NODE_ENV === 'production',
+        secure : false,
         maxAge: 60 * 60 * 24 * 7, // 1 week
         path: "/",
     });
     cookies().set("session_access_token", accessToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        // secure: process.env.NODE_ENV === 'production',
+        secure : false,
         maxAge: 60 * 60, // 60 mins
         path: "/",
     });
     cookies().set("session_refresh_token", refreshToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        // secure: process.env.NODE_ENV === 'production',
+        secure : false,
         maxAge: 60 * 60 * 24 * 7, // 1 week
         path: "/",
     });
